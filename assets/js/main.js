3a639373c112b87d3544fdfa70ef8f85b61d1260
lxMBIT <lxmbit@gmail.com>
2015-10-20 17:22:28 +0300

    center image

--- assets/js/main.js
+++ assets/js/main.js
@@ -435,7 +435,7 @@ var main = (function($) { var _ = {
  							// Set background stuff.
 	 							s.$slideImage
 		 							.css('background-image', '')
-		 							.css('background-position', ('center'));
+		 							.css('background-position', ($thumbnail.data('position') || 'center'));
 
 						// Caption.
 							s.$slideCaption = s.$slide.find('.caption');
